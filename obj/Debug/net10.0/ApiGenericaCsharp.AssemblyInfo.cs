1c9c5cbafd3e6eac45c8bd7a4e52a78065a5de15
Koriies <kevin.studiante@gmail.com>
2026-04-23 17:30:12 -0500

    Crear RepositorioLecturaPostgreSQL con detección automática de tipos

--- obj/Debug/net10.0/ApiGenericaCsharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/ApiGenericaCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5087d2b580f012cce9e92157a9c5708819f13387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f7e0c5d607c3e971daa64ef249c962fba06690")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
